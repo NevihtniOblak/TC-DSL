bffb0f25f90bbb4275b578d28ff8315b9e86964c
NevihtniOblak <147940911+NevihtniOblak@users.noreply.github.com>
2024-06-08 13:46:35 +0200

    feat: dodal ogrodja vseh produkcij (TC-155)

--- src/Main.kt
+++ src/Main.kt
@@ -4,13 +4,20 @@ import SyntaxAnalysis.Recognizer
 import java.io.File
 
 fun main(args: Array<String>) {
-    //val input = File(args[0]).readText()
-    //val outputFile = File(args[1])
+    val input = File(args[0]).readText()
+    val outputFile = File(args[1])
 
     //Lexer(ForForeachFFFAutomaton, "SCHEMA-for+for130+-*\"oo1!?\"// / ^[]displayMarkerscallprint for=varCirclePolygonLineBoxsetMarker rotatetranslate;;setLocation<>ParkBuilding-ComplexShop-Mercator Shop-Tus Path Aqua Rail Road Building:City,{}(()SCHEMA) procedureSCHEMASCHEMA".byteInputStream()).printTokens(System.out)
     //Lexer(ForForeachFFFAutomaton, "for".byteInputStream()).printTokens(System.out)
     //Lexer(ForForeachFFFAutomaton,  input.byteInputStream()).printTokens(outputFile.outputStream())
 
-    Recognizer(Lexer(ForForeachFFFAutomaton,  "for var i".byteInputStream())).recognizeStart()
+    val result = Recognizer(Lexer(ForForeachFFFAutomaton,  input.byteInputStream())).recognizeStart()
+
+    if(result){
+        print("The input is a valid program")
+    }
+    else{
+        print("The input is NOT a valid program")
+    }
 
 }
--- src/SyntaxAnalysis/Recognizer.kt
+++ src/SyntaxAnalysis/Recognizer.kt
@@ -10,6 +10,7 @@ class Recognizer(private val lexer: Lexer) {
 
     fun recognizeStart(): Boolean {
         currentSymbol = lexer.getToken()
+        //println("Current symbol: "+currentSymbol)
         val result = recognizePROGRAM();
 
         return when (currentSymbol?.symbol) {
@@ -32,6 +33,163 @@ class Recognizer(private val lexer: Lexer) {
 
     fun recognizePROGRAM(): Boolean {
         return true
+    }
+
+
+    fun recognizePREDEF(): Boolean {
+        return true
+    }
+
+    fun recognizeSCHEMAS(): Boolean {
+        return true
+    }
+
+    fun recognizeSCHEMAS2(): Boolean {
+    return true
+}
+
+    fun recognizePROCEDURE(): Boolean {
+        return true
+    }
+
+    fun recognizeARGUMENTS(): Boolean {
+        return true
+    }
+
+    fun recognizeARGUMENTS2(): Boolean {
+    return true
+    }
+
+    fun recognizeCITY(): Boolean {
+        return true
+    }
+
+    fun recognizeCOMPONENTS(): Boolean {
+        return true
+    }
+
+    fun recognizeINFRASTRUCTURE(): Boolean {
+        return true
+    }
+
+    fun recognizeINFNAMES(): Boolean {
+        return true
+    }
+
+    fun recognizeCONTAINERS(): Boolean {
+        return true
+    }
+
+    fun recognizeCONTNAMES(): Boolean {
+        return true
+    }
+
+    fun recognizeREF(): Boolean {
+        return true
+    }
+
+    fun recognizeTAG(): Boolean {
+        return true
+    }
+
+    fun recognizeRENDER(): Boolean {
+        return true
+    }
+
+    fun recognizeRENDERCONT(): Boolean {
+        return true
+    }
+
+    fun recognizeEFFECT(): Boolean {
+        return true
+    }
+
+    fun recognizeCOMMANDS(): Boolean {
+        return true
+    }
+
+    fun recognizeSPECS(): Boolean {
+        return true
+    }
+
+    fun recognizePOLYARGS(): Boolean {
+        return true
+    }
+
+    fun recognizePOLYARGS2(): Boolean {
+    return true
+    }
+
+    fun recognizeSTMTS(): Boolean {
+        return true
+    }
+
+    fun recognizeASSIGNS(): Boolean {
+        return true
+    }
+
+    fun recognizeCONSTRUCTNAMES(): Boolean {
+        return true
+    }
+
+    fun recognizeEXP(): Boolean {
+        return true
+    }
+
+    fun recognizeADDITIVE(): Boolean {
+        return true
+    }
+
+    fun recognizeADDITIVE2(): Boolean {
+    return true
+    }
+
+    fun recognizeMULTIPLICATIVE(): Boolean {
+        return true
+    }
+
+    fun recognizeMULTIPLICATIVE2(): Boolean {
+    return true
+    }
+
+    fun recognizeEXPONENTIAL(): Boolean {
+        return true
+    }
+
+    fun recognizeEXPONENTIAL2(): Boolean {
+    return true
+    }
+
+    fun recognizeUNARY(): Boolean {
+        return true
+    }
+
+    fun recognizePRIMARY(): Boolean {
+        return true
+    }
+
+    fun recognizePRIMARY1(): Boolean {
+        return true
+    }
+
+    fun recognizePRIMARY2(): Boolean {
+        return true
+    }
+
+    fun recognizeDATA(): Boolean {
+        return true
+    }
+
+    fun recognizeLIST(): Boolean {
+        return true
+    }
+
+    fun recognizeLISTITEM(): Boolean {
+        return true
+    }
+
+    fun recognizeLISTITEM2(): Boolean {
+    return true
     }
 
 }
